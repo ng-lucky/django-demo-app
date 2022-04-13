4a2f196d26f320ad16a58776d11dd4cb950f407d
Plutocracy <luckydogbey@gmail.com>
2022-04-13 06:53:31 +0000

    added migrations

--- main/models.py
+++ main/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
-
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    photo_url = models.CharField(max_length=2000, default='', blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    
 class TestModel(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
